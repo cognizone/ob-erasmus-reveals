205f0c0ee8a15009fe231a5d66fd5f918852aa5c
William Chelman <william.chelman@cogni.zone>
2022-11-22 17:10:49 +0100

    chore: initial setup

---
+++ frontend/apps/ob-erasmus-reveal/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ob-erasmus-reveal-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'ob-erasmus-reveal';
+}
