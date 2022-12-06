ab9406fabd2e7b67c3a93f94426829cae709366d
srikanthv02 <srikanthv02@gmail.com>
2022-12-06 11:01:42 +0100

    adding images for homepage

--- frontend/apps/ob-erasmus-reveal/src/app/features/home/home.component.ts
+++ frontend/apps/ob-erasmus-reveal/src/app/features/home/home.component.ts
@@ -1,8 +1,38 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'ob-erasmus-reveal-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent {}
+export class HomeComponent {
+  targets: Target[] = [
+    {
+      'altLabel': 'target_1.image_alt',
+      'label': 'target_1.label',
+      'path': 'assets/images/icon-svgs/icon-view.svg',
+      'description': 'target_1.description'
+    },
+    {
+      'altLabel': 'target_2.image_alt',
+      'label': 'target_2.label',
+      'path': 'assets/images/icon-svgs/icon-rating.svg',
+      'description': 'target_2.description'
+    },
+    {
+      'altLabel': 'target_3.image_alt',
+      'label': 'target_3.label',
+      'path': 'assets/images/icon-svgs/icon-award.svg',
+      'description': 'target_3.description'
+    }
+  ]
+
+}
+
+interface Target {
+  altLabel: string;
+  label: string;
+  path: string;
+  description: string;
+}
--- frontend/apps/ob-erasmus-reveal/src/app/shared/components/about/about.component.ts
+++ frontend/apps/ob-erasmus-reveal/src/app/shared/components/about/about.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'ob-erasmus-reveal-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AboutComponent {}
