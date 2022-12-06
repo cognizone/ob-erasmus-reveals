5ef1370bee415b5a889907a1b85ab1aba46d0bd7
srikanthv02 <srikanthv02@gmail.com>
2022-12-06 11:00:49 +0100

    feat: Homepage setup Reveal
    see issue - #T15184

--- frontend/apps/ob-erasmus-reveal/src/app/app.module.ts
+++ frontend/apps/ob-erasmus-reveal/src/app/app.module.ts
@@ -5,10 +5,25 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, CoreModule, SharedModule.forRoot()],
+  imports: [
+      BrowserModule,
+      BrowserAnimationsModule,
+      CoreModule,
+      SharedModule.forRoot(),
+      RouterModule.forRoot(
+        [
+          {
+            path: '',
+            pathMatch: 'full',
+            loadChildren: () => import('./features/home/home.module').then(m => m.HomeModule)
+          }
+        ]
+    ),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
