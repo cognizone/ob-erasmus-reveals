1cc9d61f68b623c25d4f2efab655a78de1f05091
christina_spachiou <christina.spachiou@cogni.zone>
2023-04-19 11:21:56 +0200

    unused import

--- backend/src/main/java/zone/cogni/reveal/email/EmailController.java
+++ backend/src/main/java/zone/cogni/reveal/email/EmailController.java
@@ -1,7 +1,6 @@
 package zone.cogni.reveal.email;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +14,6 @@ import zone.cogni.reveal.model.SignupModel;
 
 @RestController
 @RequiredArgsConstructor
-@Slf4j
 public class EmailController {
   private final EmailService emailService;
 
